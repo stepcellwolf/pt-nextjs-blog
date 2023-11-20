6ae3310915e529e99b6da9cae9eb6c99115afe52
Predrag Tasevski <stepcellwolf@gmail.com>
2023-11-20 19:21:26 +0100

    typo in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
     <footer>
       <div className="mt-16 flex flex-col items-center">
         <div className="mb-3 flex space-x-4">
-          Coonect with me via: <br />
+          Connect with me via: <br />
           <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
           <SocialIcon kind="github" href={siteMetadata.github} size={6} />
           <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
