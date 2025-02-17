c5db3090056de21f967cd7113e86688ff5d59fd4
Predrag Tasevski <p.tasevski@unicis.tech>
2025-02-17 21:33:26 +0100

    fix 2

--- next.config.js
+++ next.config.js
@@ -1,55 +1,37 @@
-const { withContentlayer } = require('next-contentlayer')
+const { withContentlayer } = require('next-contentlayer');
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
+});
 
-/**
- * @type {import('next/dist/next-server/server/config').NextConfig}
- **/
-module.exports = () => {
-  const plugins = [withContentlayer, withBundleAnalyzer]
-  return plugins.reduce((acc, next) => next(acc), {
-    reactStrictMode: true,
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-    eslint: {
-      dirs: ['app', 'components', 'layouts', 'scripts'],
-    },
-    images: {
-      domains: ['picsum.photos'],
-    },
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          // headers: securityHeaders,
-        },
-      ]
-    },
-    webpack: (config, options) => {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      })
-
-      return config
-    },
-  })
-}
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
+  reactStrictMode: true,
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  eslint: {
+    dirs: ['app', 'components', 'layouts', 'scripts'],
   },
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
+  images: {
+    domains: ['picsum.photos'],
+    unoptimized: true, // Required for GitHub Pages
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        // headers: securityHeaders,
+      },
+    ];
+  },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
+    return config;
+  },
+  output: 'export', // Required for static export
+};
 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
-
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(withContentlayer(nextConfig));
