f888445c45541abc19b81b6e1dd455dd6a7c7012
Predrag Tasevski <p.tasevski@unicis.tech>
2025-02-20 21:29:42 +0100

    add goatcounter

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,6 +94,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
+      <script
+        data-goatcounter="https://nextjs-pt-blog.goatcounter.com/count"
+        async
+        src="//gc.zgo.at/count.js"
+      ></script>
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
